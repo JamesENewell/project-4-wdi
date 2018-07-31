1c58384473ea6a557a84f627c69c1c0a6e0661a1
dcravenor <happy-james@hotmail.co.uk>
2018-07-31 15:17:28 +0100

    reinstated seeds and changed config environment

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,6 @@
-const dbURI = 'mongodb://localhost/codeshop';
 const port = process.env.PORT || 4000;
+const env = process.env.NODE_ENV || 'dev';
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/codeshop-api-${env}`;
 const secret = 'hellotheregeneralkenobi';
 
 module.exports = { dbURI, port, secret };
